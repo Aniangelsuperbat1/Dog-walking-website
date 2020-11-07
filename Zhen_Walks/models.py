8192da4df229226e55576f6100fe641df44d4e1f
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-11-07 12:20:39 -0500

    added contact database.

--- Zhen_Walks/models.py
+++ Zhen_Walks/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-# class contact(models.Model):
-#     name = models.CharField(max_length=50)
-#     email = models.EmailField()
-#     phone = models.CharField(max_length=10)
-#     Opinion = models.TextField()
-    
 
-
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    desc = models.TextField()
--- Zhen_Walks/views.py
+++ Zhen_Walks/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from Zhen_Walks.models import Contact
 
 
 # Create your views here.
@@ -13,6 +14,15 @@ def about(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        desc = request.POST["desc"]
+        print(name, email, desc)
+
+        data = Contact(name=name, email=email, desc=desc)
+        data.save()
+
     return render(request, "contact.html")
 
 
