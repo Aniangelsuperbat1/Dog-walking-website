06c5c60a31c956d340f5d8d63a90b060a720dda9
Patrick Zhen <Zhen.Patrick@gmail.com>
2020-11-07 12:21:54 -0500

    added database.

--- Zhen_Walks/admin.py
+++ Zhen_Walks/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
+from Zhen_Walks.models import Contact
 # from Zhen_Walks.models import contact
 
 # Register your models here.
 
-# admin.site.register(contact)
-
-
-
-
+admin.site.register(Contact)
